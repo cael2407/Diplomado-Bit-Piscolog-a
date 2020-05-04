f9e708ff893c78385450b3fcc4adb21bf04d37cf
cael2407 <cael.2407@gmail.com>
2020-05-04 01:12:38 -0500

    Trabajo Final

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+
+
+function nombreSitioWeb() {
+    var nombreSitio =prompt("¿Cómo se llama este sitio web?");
+
+if (nombreSitio == "Piscología y Estudio") {
+  alert ("El nombre es correcto");  
+} else {
+    alert ("El nombre es incorrecto");
+    }
+}
+
+function confirmacion() {
+    return confirm ("¿Quieres ir a la red social?")
+}
